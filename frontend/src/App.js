77b56fb1d5cbf2eb2e2dbce08061903aafeeba2c
kjy <kjo5655@buptle.com>
2021-08-07 12:30:12 +0900

    project start

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={()=>{
+        alert('aaaaaaaaa')}
+        }>
+          aaa
+        </button>
+          Learn Reactaaaaaaaaaaaaaaaaaaaaaaaa
+      </header>
+    </div>
+  );
+}
+
+export default App;
