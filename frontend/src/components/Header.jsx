e02496d5ef58dfa83f5180112cb98bfdeb026668
benevolentroad <6enevolentroad@gmail.com>
2021-08-17 03:35:54 +0900

    .header -> #header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ import KakaoSignUp from "./KakaoSignUp";
 
 export default function Header() {
   return (
-    <header className="header">
+    <header id="header">
       <h1>
         <NavLink to="/">
           <img src={logo} className="logo" alt="logo" />
